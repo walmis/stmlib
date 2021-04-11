46c80fb420f8d34bea22598819477791b613b91e
Valmantas Paliksa <walmis@gmail.com>
2021-04-11 13:42:00 +0300

    I2c Update

--- src/i2cbb.c
+++ src/i2cbb.c
@@ -3,18 +3,30 @@
 #include <libopencm3/cm3/dwt.h>
 #include <libopencm3/cm3/cortex.h>
 
+#pragma GCC optimize("O3")
+
 #define SDA_LOW() GPIO_BRR(i2c->port) = i2c->sda
 #define SDA_HIGH() GPIO_BSRR(i2c->port) = i2c->sda
-
 #define SCL_LOW() GPIO_BRR(i2c->port) = i2c->scl
 #define SCL_HIGH() GPIO_BSRR(i2c->port) = i2c->scl
-
 #define SDA_READ() (GPIO_IDR(i2c->port) & i2c->sda)
 
+#define CPU_MHZ 72
+
+#define DELAY_LOW_US  0.8 //μs
+#define DELAY_HIGH_US 0.45 //μs
+
+#define DELAY_CYCLES_LOW  ((int)(DELAY_LOW_US/(1.0/CPU_MHZ))-14)
+#define DELAY_CYCLES_HIGH ((int)((DELAY_HIGH_US)/(1.0/CPU_MHZ))-14)
+
+#undef CM_ATOMIC_BLOCK
+#define CM_ATOMIC_BLOCK()
+
 //wait for 1/2 of I2C clock period
-static inline void i2c_bb_hc(void){
- uint32_t s = DWT_CYCCNT;
- while(DWT_CYCCNT - s < (72/5)) {}
+static inline void i2c_bb_hc(int cycles){
+  if(cycles <= 0) return;
+  uint32_t t = DWT_CYCCNT;
+  while(DWT_CYCCNT-t<cycles);
 }
 
 //setup bit bang I2C pins
@@ -41,143 +53,144 @@ void i2c_bb_setup(const struct i2cbb* i2c){
 	if(SDA_READ() == 0) {
 		for(int i = 0; i < 8; i++) {
 			SCL_LOW();
-			i2c_bb_hc();
+			i2c_bb_hc(DELAY_CYCLES_LOW);
 			SCL_HIGH();
-			i2c_bb_hc();
+			i2c_bb_hc(DELAY_CYCLES_LOW);
 		}
 	}
 }
 
-
-
 static void i2c_bb_start(const struct i2cbb* i2c){
-  i2c_bb_hc();
+  i2c_bb_hc(DELAY_CYCLES_LOW);
 
   SCL_HIGH();
+  SDA_HIGH();
+
   //wait for 1/2 clock first
-  i2c_bb_hc();
+  i2c_bb_hc(DELAY_CYCLES_LOW);
   //pull SDA low
   SDA_LOW();
   //wait for 1/2 clock for end of start
-  i2c_bb_hc();
+  i2c_bb_hc(DELAY_CYCLES_LOW);
+
+  SCL_LOW();
+
+  i2c_bb_hc(DELAY_CYCLES_LOW);
 }
 
 static void i2c_bb_stop(const struct i2cbb* i2c){
   //pull SDA low
   SDA_LOW();
   //wait for 1/2 clock for end of start
-  i2c_bb_hc();
+  i2c_bb_hc(DELAY_CYCLES_LOW);
   //float SCL
   SCL_HIGH();
   //wait for 1/2 clock
-  i2c_bb_hc();
+  i2c_bb_hc(DELAY_CYCLES_HIGH);
   //float SDA
   SDA_HIGH();
   //wait for 1/2 clock
-  i2c_bb_hc();
+  i2c_bb_hc(DELAY_CYCLES_LOW);
 
 }
 
 //send value over I2C return 1 if slave ACKed
 static short i2c_bb_tx_byte(const struct i2cbb* i2c, unsigned char val){
   int i;
-  cm_disable_interrupts();
-  //shift out bits
-  for(i=0;i<8;i++){
+  //cm_disable_interrupts();
+  CM_ATOMIC_BLOCK() {
+    //shift out bits
+    for(i=0;i<8;i++){
+
+      //check bit
+      if(val&0x80){
+        //float SDA
+        SDA_HIGH();
+      }else{
+        //pull SDA low
+        SDA_LOW();
+      }
+      //shift
+      val<<=1;
+      //wait for 1/2 clock
+      i2c_bb_hc(DELAY_CYCLES_LOW-18);
+      //float SCL
+      SCL_HIGH();
+      i2c_bb_hc(DELAY_CYCLES_HIGH-4);
+      //pull SCL low
+      SCL_LOW();
+
+    }
+    //check ack bit
     //pull SCL low
     SCL_LOW();
-    //check bit
-    if(val&0x80){
-      //float SDA
-      SDA_HIGH();
-    }else{
-      //pull SDA low
-      SDA_LOW();
-    }
-    //shift
-    val<<=1;
+    //float SDA
+    SDA_HIGH();
     //wait for 1/2 clock
-    i2c_bb_hc();
+    i2c_bb_hc(DELAY_CYCLES_LOW);
     //float SCL
     SCL_HIGH();
     //wait for 1/2 clock
-    i2c_bb_hc();
+    i2c_bb_hc(DELAY_CYCLES_HIGH);
+    //sample SDA
+    val= SDA_READ();
+    //pull SCL low
+    SCL_LOW();
   }
-  //check ack bit
-  //pull SCL low
-  SCL_LOW();
-  //float SDA
-  SDA_HIGH();
-  //wait for 1/2 clock
-  i2c_bb_hc();
-  //float SCL
-  SCL_HIGH();
-  //wait for 1/2 clock
-  i2c_bb_hc();
-  //sample SDA
-  val= SDA_READ();
-  //pull SCL low
-  SCL_LOW();
-  //return sampled value
-  cm_enable_interrupts();
   return val == 0;
 }
 //send value over I2C return 1 if slave ACKed
 unsigned char i2c_bb_rx_byte(const struct i2cbb* i2c, unsigned short ack){
   unsigned char val;
   int i;
-  cm_disable_interrupts();
-  //shift out bits
-  for(i=0;i<8;i++){
+  CM_ATOMIC_BLOCK() {
+    //shift out bits
+    for(i=0;i<8;i++){
+      //pull SCL low
+      SCL_LOW();
+      //SCL_LOW();
+
+      //wait for 1/2 clock
+      i2c_bb_hc(DELAY_CYCLES_LOW-4);
+      //float SCL
+      val<<=1;
+      SCL_HIGH();
+      //wait for 1/2 clock
+      i2c_bb_hc(DELAY_CYCLES_HIGH-20);
+      //shift value to make room
+
+      //sample data
+      if(SDA_READ()){
+        val|=1;
+      }
+
+      //pull SCL low
+      SCL_LOW();
+
+    }
+    //check ack bit
     //pull SCL low
     SCL_LOW();
+    //check if we are ACKing this byte
+    if(ack){
+      //pull SDA low for ACK
+      SDA_LOW();
+    }else{
+      //float SDA for NACK
+      SDA_HIGH();
+    }
     //wait for 1/2 clock
-    i2c_bb_hc();
+    i2c_bb_hc(DELAY_CYCLES_LOW);
     //float SCL
     SCL_HIGH();
     //wait for 1/2 clock
-    //i2c_bb_hc();
-
-    #pragma GCC unroll 30
-    for(int i = 0; i < 4; i++) {
-    	asm("nop");
-    }
-
-    //shift value to make room
-    val<<=1;
-    //sample data
-    if(SDA_READ()){
-      val|=1;
-    }
-
+    i2c_bb_hc(DELAY_CYCLES_HIGH);
     //pull SCL low
     SCL_LOW();
-
-
-  }
-  //check ack bit
-  //pull SCL low
-  SCL_LOW();
-  //check if we are ACKing this byte
-  if(ack){
-    //pull SDA low for ACK
-    SDA_LOW();
-  }else{
-    //float SDA for NACK
+    //float SDA
     SDA_HIGH();
-  }
-  //wait for 1/2 clock
-  i2c_bb_hc();
-  //float SCL
-  SCL_HIGH();
-  //wait for 1/2 clock
-  i2c_bb_hc();
-  //pull SCL low
-  SCL_LOW();
-  //float SDA
-  SDA_HIGH();
 
-  cm_enable_interrupts();
+  }
   //return value
   return val;
 }
